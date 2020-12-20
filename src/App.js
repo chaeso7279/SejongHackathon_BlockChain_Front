38b557827edf8125226c8f6302fc1e6916feeb67
sujin22 <aoghksj@naver.com>
2020-12-21 03:52:33 +0900

    페이지 및 컴포넌트 파일 생성, Route 선언

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import {HashRouter, Route} from 'react-router-dom';
+import Main from './routes/Main';
+import MyShareList from './routes/MyShareList';
+import MyPage from'./routes/MyPage';
+import MileageApplication from'./routes/MileageApplication';
+import ShareItemList from './routes/ShareItemList';
+import ShareItemDetail from './routes/ShareItemDetail';
+import ShareTalentList from './routes/ShareTalentList';
+import ShareTalentDetail from './routes/ShareTalentDetail';
+import Navigation from './components/Navigation';
 
 function App() {
+  let router;
+  const navigate = (pageName = '') =>{
+    router.history.push('/'+pageName);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <HashRouter ref = {(r)=>{router = r;}}>
+        <Route path="/" exact={true} component={Main}/>
+        <Route path="/mysharelist" component={MyShareList}/>
+        <Route path="/mileage_application" component={MileageApplication}/>
+        <Route path="/mypage" component={MyPage}/>
+
+        <Route path="/share_item_list" component={ShareItemList}/>
+        <Route path="/share_item_detail" component={ShareItemDetail}/>
+        <Route path="/share_talent_list" component={ShareTalentList}/>
+        <Route path="/share_talent_detail" component={ShareTalentDetail}/>
+      </HashRouter>
   );
 }
 
