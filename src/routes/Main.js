a95caccd503f99f7471da0e7f701cbeaaf33e107
sujin22 <aoghksj@naver.com>
2020-12-22 01:32:12 +0900

    임시

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './Main.css'
 import TopBar from '../components/TopBar'
+import main_banner from '../images/main_banner.png'
 
 class Main extends React.Component{
     render(){
         return(
             <div className="Main">
-              <div className="topbar">
+                <div className="topbar">
                     <TopBar/>
+                </div>
+                <div className="main_banner">
+                    <image src= {main_banner} alt="MainBanner"/>
                 </div>
             </div>
             
