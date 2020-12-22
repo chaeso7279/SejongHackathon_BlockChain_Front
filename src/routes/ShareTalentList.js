2670f33092e473e01c98ea7e6e1990eef6673213
OhHyerin <44857166+OhHyerin@users.noreply.github.com>
2020-12-22 11:48:51 +0900

    ShareTalentList

--- src/routes/ShareTalentList.js
+++ src/routes/ShareTalentList.js
@@ -3,8 +3,11 @@ import LiginBar from '../components/LoginBar';
 import './ShareTalentList.css';
 import {Link, Switch} from 'react-router-dom';
 import LoginBar from '../components/LoginBar';
+import Navigation from '../components/Navigation';
+
 
 import banner from '../IMG/talentbanner.png';
+import img1 from '../IMG/talent_img1.png';
 
 
 class ShareTalentList extends React.Component{
@@ -14,22 +17,30 @@ class ShareTalentList extends React.Component{
 
     }
 
-
-    Banner = () => {
-        <div className="talent_banner">
-            <img src="../IMG/talentbanner"></img>
-        </div>
-    }
-
     render(){
         return(
             <div>
-                {/* <div className="header"> */}
-                {/* </div> */}
-
-                <div className="talent_banner">
-                  <img src={banner}></img>
+                <div className="header">
+                    <LoginBar />
+                    <Navigation />
                 </div>
+                <div className="talent_banner">
+                    <img src={banner}></img>
+                </div>
+                    
+                <div className="talent_canvas">
+                    <div className="img">
+                        <div className="content">
+                            <h4>비교과 마일리지는 </h4>
+                            <h2>총 기여 시간</h2>
+                        </div>
+                        <div class="img-cover"></div>
+
+
+                    </div>
+                    {/* <img src={img1}></img>
+                    <span>비교과 마일리지는</span> */}
+                </div>  {/*talent_canvas*/}
 
             </div>
         );
