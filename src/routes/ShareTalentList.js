d01ea673eac0cc070d329c56ede6fd8b6101fb8e
OhHyerin <44857166+OhHyerin@users.noreply.github.com>
2020-12-22 14:52:06 +0900

    ...

--- src/routes/ShareTalentList.js
+++ src/routes/ShareTalentList.js
@@ -8,6 +8,7 @@ import Navigation from '../components/Navigation';
 
 import banner from '../IMG/talentbanner.png';
 import img1 from '../IMG/talent_img1.png';
+import { Button } from '@material-ui/core';
 
 
 class ShareTalentList extends React.Component{
@@ -16,6 +17,7 @@ class ShareTalentList extends React.Component{
         //데이터 로딩
 
     }
+
 
     render(){
         return(
@@ -29,17 +31,17 @@ class ShareTalentList extends React.Component{
                 </div>
                     
                 <div className="talent_canvas">
-                    <div className="img">
-                        <div className="content">
-                            <h4>비교과 마일리지는 </h4>
-                            <h2>총 기여 시간</h2>
+                    <div className="talent_img">
+                        <div className="talent_txt">
+                            <h5>비교과 마일리지는 총 기여 시간과 기부 받은 사람에게 받은 점수에 비례하여 적립됩니다.</h5>
+                            <h5>비교과 활동과 무관한 재능 교환은 마일리지 적립이 어려울 수 있습니다.</h5>
                         </div>
-                        <div class="img-cover"></div>
-
-
                     </div>
-                    {/* <img src={img1}></img>
-                    <span>비교과 마일리지는</span> */}
+                    <Link to="/share_talent_list">
+                        <button className="talent_btn_add"> 재능 올리기
+                        </button>
+                    </Link>
+                    
                 </div>  {/*talent_canvas*/}
 
             </div>
